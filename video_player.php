df1c6a0b8dd398d44d0028b539404666f0cdc038
Jhersy <jhersy1994@gmail.com>
2017-11-03 19:10:03 +0100

    Implementación omentarios de un vídeo. Vista en listado

--- video_player.php
+++ video_player.php
@@ -1,132 +1,76 @@
 <?php
 
-require_once("src/App.php");
-$rol = isAdmin(); //Return session admin or null
 
-
-/**
- * Library Requirements
- *
- * 1. Install composer (https://getcomposer.org)
- * 2. On the command line, change to this directory (api-samples/php)
- * 3. Require the google/apiclient library
- *    $ composer require google/apiclient:~2.0
- */
 if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
-  throw new \Exception('please run "composer require google/apiclient:~2.0" in "' . __DIR__ .'"');
-}
-
-require_once __DIR__ . '/vendor/autoload.php';
-session_start();
-
-/*
- * You can acquire an OAuth 2.0 client ID and client secret from the
- * {{ Google Cloud Console }} <{{ https://cloud.google.com/console }}>
- * For more information about using OAuth 2.0 to access Google APIs, please see:
- * <https://developers.google.com/youtube/v3/guides/authentication>
- * Please ensure that you have enabled the YouTube Data API for your project.
- */
-$OAUTH2_CLIENT_ID = "88517581272-gu071qtdg26cg9oqbu8v3pmifgg6jogv.apps.googleusercontent.com";
-$OAUTH2_CLIENT_SECRET = "4xobKsbsIv2nFo7XOhcadA6V";
-
-$client = new Google_Client();
-$client->setClientId($OAUTH2_CLIENT_ID);
-$client->setClientSecret($OAUTH2_CLIENT_SECRET);
-$client->setScopes('https://www.googleapis.com/auth/youtube');
-$redirect = filter_var('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'],
-  FILTER_SANITIZE_URL);
-$client->setRedirectUri($redirect);
-
-$youtube = new Google_Service_YouTube($client);
-
-// Check if an auth token exists for the required scopes
-$tokenSessionKey = 'token-' . $client->prepareScopes();
-if (isset($_GET['code'])) {
-  if (strval($_SESSION['state']) !== strval($_GET['state'])) {
-    die('The session state did not match.');
+    throw new \Exception('please run "composer require google/apiclient:~2.0" in "' . __DIR__ .'"');
   }
+  
+  require_once __DIR__ . '/vendor/autoload.php';
+  session_start();
+  
+  $OAUTH2_CLIENT_ID = '88517581272-gu071qtdg26cg9oqbu8v3pmifgg6jogv.apps.googleusercontent.com';
+  $OAUTH2_CLIENT_SECRET = '4xobKsbsIv2nFo7XOhcadA6V';
+  
+ 
+  $client = new Google_Client();
+  $client->setClientId($OAUTH2_CLIENT_ID);
+  $client->setClientSecret($OAUTH2_CLIENT_SECRET);
+  
+ 
+  $client->setScopes('https://www.googleapis.com/auth/youtube.force-ssl');
+  $redirect = filter_var('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'],
+      FILTER_SANITIZE_URL);
+  $client->setRedirectUri($redirect);
+  
+  // Define an object that will be used to make all API requests.
+  $youtube = new Google_Service_YouTube($client);
+  
+  $tokenSessionKey = 'token-' . $client->prepareScopes();
+  
+  if (isset($_GET['code'])) {
+    if (strval($_SESSION['state']) !== strval($_GET['state'])) {
+      die('The session state did not match.');
+    }
+  
+    $client->authenticate($_GET['code']);
+    $_SESSION[$tokenSessionKey] = $client->getAccessToken();
+    header('Location: ' . $redirect);
+  }
+  
+  if (isset($_SESSION[$tokenSessionKey])) {
+    $client->setAccessToken($_SESSION[$tokenSessionKey]);
+  }
+  
+  
+  if ($client->getAccessToken()) {
+      $videoId = "wisbrPN9fbI";
+  
+      $listResponse = $youtube->videos->listVideos("snippet, contentDetails, statistics, player",array('id' => $videoId));
+      
+      $video = $listResponse[0];
+      $videoSnippet = $video['snippet'];
+      $videoSnippet = $video['snippet'];
+      $videoContentDetails = $video['contentDetails'];
+      $videoStatistics = $video['statistics'];
+      $videoPlayer = $video['player'];
 
-  $client->authenticate($_GET['code']);
-  $_SESSION[$tokenSessionKey] = $client->getAccessToken();
-  header('Location: ' . $redirect);
-}
-
-if (isset($_SESSION[$tokenSessionKey])) {
-  $client->setAccessToken($_SESSION[$tokenSessionKey]);
-}
-
-
-if ($client->getAccessToken()) {
-    $videoId = "wisbrPN9fbI";
-
-    // ESTADÍSTICAS DE UN VÍDEO
-    $listResponse = $youtube->videos->listVideos("snippet, contentDetails, statistics, player",
-    array('id' => $videoId));
-    $video = $listResponse[0];
-    $videoSnippet = $video['snippet'];
-    $videoContentDetails = $video['contentDetails'];
-    $videoStatistics = $video['statistics'];
-    $videoPlayer = $video['player'];
-
-
-    //LISTA DE COMENTARIOS DE UN VÍDEO
-/*
-    try {
-    //$videoCommentThreads = $youtube->commentThreads->listCommentThreads("snippet, replies", array('videoId' => $videoId));
-    $videoCommentThreads = $youtube->commentThreads->listCommentThreads('snippet', array(
+      // ESTADÍSTICAS DE UN VÍDEO
+      $videoCommentThreads = $youtube->commentThreads->listCommentThreads('snippet, replies', array(
         'videoId' => $videoId,
         'textFormat' => 'plainText',
         ));
-    
-    $parentId = $videoCommentThreads[0]['id'];
-
-    $videoComments = $youtube->comments->listComments('snippet', array(
-        'parentId' => $parentId,
-        'textFormat' => 'plainText',
-    ));
-
-    if (empty($videoComments)) {
-        $htmlBody .= "<h3>Can\'t get video comments.</h3>";
-      } else {
-        $videoComments[0]['snippet']['textOriginal'] = 'updated';
-        $htmlBody1 .= "<h3>Video Comment Replies</h3><ul>";
-        foreach ($videoComments as $comment) {
-          $htmlBody1 .= sprintf('<li>%s: "%s"</li>', $comment['snippet']['authorDisplayName'],
-              $comment['snippet']['textOriginal']);
-        }
-        $htmlBody1 .= '</ul>';
-      }
-
-
-    } catch (Google_Service_Exception $e) {
-        $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
-            htmlspecialchars($e->getMessage()));
-    }
-*/
-    /*
-    $htmlBody .= "<h3>Video Updated</h3><ul>";
-    $htmlBody .= sprintf('<li>Tags "%s" and "%s" added for video %s (%s) </li>',
-        array_pop($responseTags), array_pop($responseTags),
-        $videoId, $video['snippet']['title']);
-        $htmlBody .= '</ul>';
-        $_SESSION['token'] = $client->getAccessToken();
-        */
-    
-}else{
-    $state = mt_rand();
-    $client->setState($state);
-    $_SESSION['state'] = $state;
-
-    $authUrl = $client->createAuthUrl();
-    echo $authUrl;
-    $htmlBody = <<<END
-<h3>Authorization Required</h3>
-<p>You need to <a href="$authUrl">authorize access</a> before proceeding.<p>
-END;
-
-}
+      
+  }else{
+      $state = mt_rand();
+      $client->setState($state);
+      $_SESSION['state'] = $state;
+  
+      $authUrl = $client->createAuthUrl();
+      echo $authUrl;  
+  }
 
 ?>
+
 
 
 <!DOCTYPE HTML>
@@ -279,36 +223,29 @@ END;
                             <span class="label label-danger"><?=$tag?></span>
                        <?php }
                     ?>
-
                     <hr class="major" />
 
                     <h4><strong> <?=$videoStatistics['commentCount']?> Comentarios</strong></h4>
-                    <img src="<?=$imagenComentario?>" alt="">
                     <dl>
-                        <dt><?=$htmlBody1?></dt>
+                    <?php 
+                    
+                    foreach ($videoCommentThreads as $comment) { ?>
+                        <dt><img class="img-circle" src="<?=$comment['snippet']['topLevelComment']['snippet']['authorProfileImageUrl']?>" alt="foto perfil usuario comentario"> <?=$comment['snippet']['topLevelComment']['snippet']['authorDisplayName']?></dt>
                         <dd>
-                            <p>Lorem ipsum dolor vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu
-                                felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent.
-                                Lorem ipsum dolor.</p>
-                        </dd>
-                        <dt>Item2</dt>
-                        <dd>
-                            <p>Lorem ipsum dolor vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu
-                                felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent.
-                                Lorem ipsum dolor.</p>
-                        </dd>
-                        <dt>Item3</dt>
-                        <dd>
-                            <p>Lorem ipsum dolor vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu
-                                felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent.
-                                Lorem ipsum dolor.</p>
-                        </dd>
+                            <p><?=$comment['snippet']['topLevelComment']['snippet']['textDisplay']?></p>
+                        </dd>           
+
+                    <?php
+                      }
+                    ?>
+                    
                     </dl>
+                <hr class="major" />
+                    
                 </div>
 
             </div>
         </div>
-
 
     </div>
 
